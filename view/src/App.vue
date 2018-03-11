8d6a358b25d7fefcf96c3ebb2b550d62619fc375
nyker510 <takamail53@gmail.com>
2018-03-11 23:51:05 +0900

    [Add] 可視化のための html ファイル作成用のスクリプト

---
+++ view/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <graph-view></graph-view>
+  </div>
+</template>
+
+<script>
+import GraphView from './components/GraphView'
+
+export default {
+  name: 'App',
+  components: {
+    GraphView
+  }
+}
+</script>
